7968ab1b826c65b113529af17da5dbfcd843d6ef
pykretexd <landysixten@gmail.com>
2024-12-25 19:54:25 +0100

    Removed config file

--- src/main.py
+++ src/main.py
@@ -1,87 +1,34 @@
 from datetime import datetime
 import logging
-import logging.config
+import logging.handlers
 import os
-import toml
+import sys
 from time import sleep
 
 from module_example import module_logging
 from module_example.sub import sub_logging
 
 
-def write_default_config(fname: str) -> None:
-    """Write default configurations to toml file."""
-    toml_string = toml.dumps({
-        'loggers': {'keys': 'root,simpleLogger'},
-        'handlers': {
-            'keys': 'consoleHandler,mainFileHandler,detectionFileHandler'},
-        'formatters': {'keys': 'simpleFormatter'},
-        'logger_root': {
-            'level': 'INFO',
-            'handlers': 'consoleHandler'
-        },
-        'logger_simpleLogger': {
-            'level': 'INFO',
-            'handlers': 'consoleHandler,mainFileHandler',
-            'qualname': 'simpleLogger',
-            'propagate': 0
-        },
-        'handler_consoleHandler': {
-            'class': 'StreamHandler',
-            'level': 'INFO',
-            'formatter': 'simpleFormatter',
-            'args': '(sys.stdout,)'
-        },
-        'handler_mainFileHandler': {
-            'class': 'handlers.TimedRotatingFileHandler',
-            'level': 'INFO',
-            'formatter': 'simpleFormatter',
-            'args': '(logs/main_log.log,midnight)'
-        },
-        'handler_detectionFileHandler': {
-            'class': 'handlers.TimedRotatingFileHandler',
-            'level': 'INFO',
-            'formatter': 'simpleFormatter',
-            'args': '(logs/detection_log.log,midnight)'
-        },
-        'formatter_simpleFormatter': {
-            'format': '[%(asctime)s] %(levelname)s - %(message)s',
-            'datefmt': '%Y/%m/%d %H:%M:%S'
-        }
-    })
-    with open(fname, 'w') as file:
-        # fileConfig refuses to work with values with quotation marks.
-        # For that reason, remove all quotation marks.
-        file.write(toml_string.replace('"', '').replace("'", ''))
-
-
 def main():
-    logging_config = 'logging.toml'
-    if not os.path.exists(logging_config):
-        write_default_config(logging_config)
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-
-    try:
-        logging.config.fileConfig(logging_config)
-    except:
-        # Configuration file was invalid. I wasn't able to find how to import
-        # configparser.InterpolationSyntaxError which can be raised if
-        # formatting was written incorrectly. A bare except was the cleanest solution.
-        write_default_config(logging_config)
-
-        # Try to read file again.
-        logging.config.fileConfig(logging_config)
     logger = logging.getLogger('example')
     logger.setLevel('INFO')
 
-    # Create file handler for logging to file.
-    # file_handler = logging.FileHandler(
-    #     '{:%Y-%m-%d}.log'.format(datetime.today()))
-    # Get formatter obtained by config file.
-    # formatter = logging.root.handlers[0].formatter
-    # file_handler.setFormatter(formatter)
-    # logger.addHandler(file_handler)
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+
+    formatter = logging.Formatter(
+        '[%(asctime)s] %(levelname)s - %(message)s',
+        datefmt='%Y/%m/%d %H:%M:%S'
+    )
+
+    # Create and apply handlers for logging to console and file.
+    console_handler = logging.StreamHandler(sys.stdout)
+    file_handler = logging.handlers.TimedRotatingFileHandler(
+        'logs/main.log', when='midnight')
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
 
     logger.debug('debug message')
     logger.info('info message')
--- src/module_example/sub.py
+++ src/module_example/sub.py
@@ -1,8 +1,30 @@
 import logging
+import logging.handlers
+import os
+import sys
+from datetime import datetime
 
 
 def sub_logging():
     logger = logging.getLogger('subLogger')
+    logger.setLevel('INFO')
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+
+    formatter = logging.Formatter(
+        '[%(asctime)s] %(levelname)s - %(message)s',
+        datefmt='%Y/%m/%d %H:%M:%S'
+    )
+
+    # Create and apply handlers for logging to console and file.
+    console_handler = logging.StreamHandler(sys.stdout)
+    file_handler = logging.handlers.TimedRotatingFileHandler(
+        'logs/sub.log', when='midnight')
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
 
     logger.debug('sub debug message')
     logger.info('sub info message')
